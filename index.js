866c6661b0a2419dc84393c9cc3fb28bc71a6869
abrshkorra <abrshkorra@gmail.com>
2023-11-18 10:58:16 +0300

    implemented google oauth w passport in express

--- index.js
+++ index.js
@@ -1,12 +1,33 @@
 const express = require('express'),
-path = require('path'),
-app = express()
+passport = require('passport')
+path = require('path')
+require('./auth')
 
+app = express()
 app.use(express.json())
 app.use(express.static(path.join(__dirname, "client")))
 
 app.get('/', (req, res)=>{
     res.sendFile('/index.html')
+})
+
+app.get('/auth/google',
+  passport.authenticate('google', { scope:
+      [ 'email', 'profile' ] }
+));
+
+app.get('/auth/google/callback',
+    passport.authenticate( 'google', {
+        successRedirect: '/auth/google/success',
+        failureRedirect: '/auth/google/failure'
+}));
+
+app.get('/auth/google/failure', (req, res) => {
+    res.send('Login failed')
+})
+
+app.get('/auth/google/sucess', (req, res) => {
+    res.send('Login sucessful')
 })
 
 app.listen(3000, ()=> {
